57d3ecd66d9db8bea796b54141a6625010aa0ed3
jhwinter <14960780+jhwinter@users.noreply.github.com>
2025-10-03 17:10:01 -0400

    Completed Iterator Pattern

--- design-patterns/src/main/java/org/prateek/BehaviouralPatterns/IteratorPattern/BookCollectionV3.java
+++ design-patterns/src/main/java/org/prateek/BehaviouralPatterns/IteratorPattern/BookCollectionV3.java
@@ -1,14 +1,11 @@
 package org.prateek.BehaviouralPatterns.IteratorPattern;
 
-
-import com.sun.source.tree.Tree;
-
 import java.util.*;
 
 public class BookCollectionV3 implements Iterable<Book>{
     private Set<Book> books = new TreeSet<>();
 
-    public void addBook(Book book){
+    public void addBook(Book book) {
         books.add(book);
     }
 
